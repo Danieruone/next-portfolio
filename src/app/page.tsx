44b14d3e3b23e7dc1c36d72ba6554d673ee34549
Daniel Mendoza <danieruone@gmail.com>
2024-09-04 22:33:04 -0500

    :fire: Remove import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // components
 import { Hero } from "@/components/Hero";
-import { NetworkChip } from "@/components/NetworkChip";
 import { NetworkSection } from "@/components/NetworkSection";
 
 export default function Home() {
