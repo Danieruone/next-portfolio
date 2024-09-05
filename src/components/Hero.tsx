1d22b6a065298e253ff0ce2e45acf7da876f1ad4
Daniel Mendoza <danieruone@gmail.com>
2024-09-04 22:12:50 -0500

    :sparkles: Add react typed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { ReactTyped } from "react-typed";
 
 export const Hero = () => {
   return (
@@ -14,7 +17,12 @@ export const Hero = () => {
       <h1 className="mt-5 mb-3 font-semibold text-5xl text-[#E2E3E3]">
         Daniel Mendoza
       </h1>
-      <p className="font-light">Software enginer / Front-end developer</p>
+      <p className="font-light">
+        <ReactTyped
+          strings={["Software engineer / Front-end developer"]}
+          typeSpeed={60}
+        />
+      </p>
     </div>
   );
 };
