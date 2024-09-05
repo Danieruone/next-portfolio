7fb8bfab1d956abbf84d88961258945b5c842ad0
Daniel Mendoza <danieruone@gmail.com>
2024-09-04 21:31:05 -0500

    :sparkles: Add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+// components
+import { Hero } from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world</div>
-    </main>
+    <div className="w-full border border-red">
+      <Hero />
+    </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <h1>Daniel Mendoza</h1>
+      <p>my portfolio</p>
+    </div>
+  );
+};
