5922d8c2272845d7dd79b34a48b3bae39da208a5
Daniel Mendoza <danieruone@gmail.com>
2024-09-04 22:24:15 -0500

    :sparkles: Add NetworkChip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 // components
 import { Hero } from "@/components/Hero";
+import { NetworkChip } from "@/components/NetworkChip";
 
 export default function Home() {
   return (
-    <div className="w-full border border-white">
+    <div className="w-full">
       <Hero />
+      <div className="flex justify-center">
+        <NetworkChip />
+        <NetworkChip />
+        <NetworkChip />
+      </div>
     </div>
   );
 }
---
+++ src/components/NetworkChip.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NetworkChip = () => {
+  return (
+    <div className="bg-[#333639] w-36 flex justify-center m-2 rounded-full p-2">
+      NetworkChip
+    </div>
+  );
+};
