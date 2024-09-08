61e7271b764a5dc0b08a0e684161d1e9455b99df
Daniel Mendoza <danieruone@gmail.com>
2024-09-08 17:03:43 -0500

    Add icon in project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+
+// icons
+import { FaGithub } from "react-icons/fa";
+import { MdWeb } from "react-icons/md";
 
 export const ProjectCard = () => {
   return (
     <div className="w-80 h-80 bg-[#333639] rounded-lg px-2 cursor-pointer transition-all hover:bg-gray-700">
       <div className="my-3">
-        <h2>My app</h2>
+        <div className="flex justify-between items-center">
+          <h2>My app</h2>
+          <div className="flex">
+            <FaGithub className="mr-2" size={20} />
+          </div>
+        </div>
         <p className="text-[.9rem] text-[#888b8f]">Description of the app</p>
       </div>
       <div className="flex justify-center m-1">
@@ -18,10 +27,10 @@ export const ProjectCard = () => {
         />
       </div>
       <div className="mt-5">
-        <h3 className="border border-gray-500 inline-block px-2 rounded-xl text-sm font-light mr-1">
+        <h3 className="border border-gray-500 inline-block px-2 rounded-xl text-sm font-light mr-1 text-[#888b8f]">
           React
         </h3>
-        <h3 className="border border-gray-500 inline-block px-2 rounded-xl text-sm font-light">
+        <h3 className="border border-gray-500 inline-block px-2 rounded-xl text-sm font-light text-[#888b8f]">
           Typescript
         </h3>
       </div>
