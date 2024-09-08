929f84ad47d054f10ec292df4e39174a9c935718
Daniel Mendoza <danieruone@gmail.com>
2024-09-07 22:30:15 -0500

    Add responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
   return (
     <div className="w-full pb-20">
       <div className="bg bg-blue-400 pt-20 pb-10 text-gray-800">
-        <Hero />
-        <NetworkSection />
+        <div className="px-5">
+          <Hero />
+          <NetworkSection />
+        </div>
       </div>
-      <div className="container mx-auto">
+      <div className="container mx-auto px-5">
         <AboutMe />
         <ProjectsSection />
         <Footer />
