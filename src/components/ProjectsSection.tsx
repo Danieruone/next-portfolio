4ee4cf371a6eb4b5faa06019ed74b64ea3789bcc
Daniel Mendoza <danieruone@gmail.com>
2024-09-08 17:22:03 -0500

    Add real projects

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,10 +6,18 @@ export const ProjectsSection = () => {
     <section className="mt-10 border-b pb-10 border-gray-500 border-1">
       <h2 className="font-light">My proyects</h2>
       <div className="mt-5 flex flex-wrap gap-5 justify-center">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          image="/projects/netflix-clone.png"
+          title="Netflix clone"
+          description="A perfect UI clone with hover video functionality."
+          techStack={["React"]}
+        />
+        <ProjectCard
+          image="/projects/music-finder.png"
+          title="Music finder"
+          description="A music finder implementing the Spotify API to search any song."
+          techStack={["React"]}
+        />
       </div>
     </section>
   );
