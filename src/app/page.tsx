40edbaba2c9dae9f1da2865ab44594184c547cf2
Daniel Mendoza <danieruone@gmail.com>
2024-09-09 19:24:22 -0500

    Change image formats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <div className="w-full pb-20">
-      <div className="relative bg-[url('/bg-code.jpg')] bg-cover pt-20 pb-10 text-gray-800">
+      <div className="relative bg-[url('/bg-code.webp')] bg-cover pt-20 pb-10 text-gray-800">
         <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
         <div className="px-5 relative z-20">
           <Hero />
